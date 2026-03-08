90bef785327b787aac0a512ca4bc733e00e08f35
Juanda Harefa <juandaharefa1@gmail.com>
2026-03-08 09:42:29 +0700

    Dasar python bagian pengenalan dan dasar python

---
+++ basic_concept.py
@@ -0,0 +1,23 @@
+"""
+Variables
+- Name to certain values
+- The values can be any data type
+- The process of storing a value inside a variable is called an assignment.
+- Can only be made up of letters, numbers, and underscore.
+- Can't be Python reserved keywords.
+"""
+
+greeting = 'Hello, World!'
+print(greeting)
+
+greeting = "hallo!"
+print(greeting)
+
+# Don't use the reserved keyword for the name of your variable
+
+def = 'Function'
+class = 'Class'
+print(and)
+print(or)
+
+# Berlaku untuk nama file --> disarankan ga boleh sama dengan reserve keyword
---
+++ pengelanan.py
@@ -0,0 +1,19 @@
+""""
+What is Python? = Python is a dynamic, interpreted (bytecode-compiled) language.
+
+Why programming with Python?
+- Easy syntax
+- Most chosen language for IT
+- Omnipresent
+- Versatile
+"""
+
+'''
+Check an installed Python
+python --version
+or 
+python3 -V
+'''
+
+# Hello world in python
+print("Hello World")
